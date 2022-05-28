903d247d23d62180afd336db5874be5d6dd503e8
MarieYee <marieyee02@yahoo.com>
2022-05-28 11:37:17 -0700

    created function

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,11 @@
 // Assignment Code
-document.querySelector("generate").addEventListener("click", writePassword)
 
 var lowercaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "W", "x", "y", "z"];
 var uppercaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numericChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialChar = ["!", "$", "-", "*", "+", "=", "@", "_",];
+
+
 
 var confirmLength = "";
 var confirmLowercaseChar;
@@ -13,6 +14,28 @@ var confirmNumericChar;
 var confirmSpecialChar;
 
 function generatePassword() {
-  var confirmLength = (prompt("How many characters do you want your password to have?"))
+  var confirmLength = (prompt("Your password can have a length of at least 8 characters and no more than 128 characters"))
 }
 
+
+
+
+
+function writePassword() {
+  var password = generatePassword();
+  var TextArea = document.getElementById("password");
+  TextArea.value = password;
+
+  }
+
+
+
+generateBtn.addEventListener("click", generatePassword);
+
+
+
+
+
+
+
+document.querySelector("#generate").addEventListener("click", writePassword)
